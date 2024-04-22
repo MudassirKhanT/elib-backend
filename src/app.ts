f4b7be480dc5db8c1e4b5f0303bb6f692b3d2d74
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-04-22 20:56:03 +0530

    create user controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandlers";
+import userRouter from "./middlewares/user/userRouter";
 const app = express();
 
 //Routes
@@ -7,6 +8,8 @@ const app = express();
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to elib apis" });
 });
+
+app.use("/api/users", userRouter);
 
 //Global error handler
 app.use(globalErrorHandler);
