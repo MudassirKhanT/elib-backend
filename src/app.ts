70a29e916bb76a3c986e4dbf53b0d5731229fdc2
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-04-22 21:04:42 +0530

    Add register user validation

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandlers";
 import userRouter from "./middlewares/user/userRouter";
 const app = express();
-
+app.use(express.json());
 //Routes
 //HTTP methods get put post delete patch
 app.get("/", (req, res, next) => {
